104eaa8167d1a3804145b05fe07d27d4abcfe02c
S0571740 <philip.blankenburg@student.htw-berlin.de>
2022-09-26 15:47:16 +0200

    remove string manipulation

--- src/main/java/ALP/KBEGateway/RabbitMQ/RabbitMQReceiver.java
+++ src/main/java/ALP/KBEGateway/RabbitMQ/RabbitMQReceiver.java
@@ -1,8 +1,5 @@
 package ALP.KBEGateway.RabbitMQ;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,28 +31,7 @@ public class RabbitMQReceiver {
                 break;
 
             case "product":
-                if (message.getValue() instanceof LinkedHashMap) {
-                    LinkedHashMap<?, ?> lhm = (LinkedHashMap<?, ?>) message.getValue();
-                    lhm.forEach((key, value) -> {
-                        if (value instanceof String) {
-                            System.out.println(key + " - " + value);
-                        } else if (value instanceof ArrayList) {
-                            System.out.print(key);
-                            for (Object obj : (ArrayList) value) {
-                                if (obj instanceof String) {
-                                    System.out.print(" - " + obj);
-                                } else if (obj instanceof LinkedHashMap) {
-                                    LinkedHashMap<String, String> linkedHashMap = (LinkedHashMap<String, String>) obj;
-                                    linkedHashMap.forEach((k, v) -> {
-                                        System.out.println(" + " + k + " - " + v);
-                                    });
-                                }
-                            }
-                        }
-                    });
-                } else {
-                ProductController.handle(message.getValue());
-                }
+                ProductController.handle((String) message.getValue());
                 break;
 
             case "price":
